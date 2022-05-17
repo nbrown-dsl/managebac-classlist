96fed28f1d34147e65f8ba9e39270f0cdd51fdfb
nbrown <nbrown@dwightlondon.org>
2022-05-17 11:51:46 +0100

    django uninstalled

--- my_app/my_app/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-from todo_list import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('todo_list.urls')),
-]
